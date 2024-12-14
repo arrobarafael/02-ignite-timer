8ce16beede186909d8d802c08f354c2c1338a853
Rafael Pelegrini <arrobarafael@hotmail.com>
2024-12-14 18:37:45 -0300

    Adicionada lista de sugestões de nomes de projetos para ser utilizado. Adicionado gap de 5 em 5 no input de minutos

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -15,9 +15,29 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="">Vou trabalhar em </label>
-          <TaskInput id="task" type="text" placeholder="Nome do projeto" />
+          <TaskInput
+            id="task"
+            list="task-suggestions"
+            type="text"
+            placeholder="Nome do projeto"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Banana" />
+          </datalist>
+
           <label htmlFor="">durante</label>
-          <MinutesAmountInput type="text" id="minutesAmount" placeholder="00" />
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
 
           <span>minutos. </span>
         </FormContainer>
