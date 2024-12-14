c46a59c3931c2a31050391e8ddbe895cc85b5175
Rafael Pelegrini <arrobarafael@hotmail.com>
2024-12-14 10:20:35 -0300

    Inicio de criação de rotas e adição de dependencia eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import { ThemeProvider } from 'styled-components';
-import { Button } from './components/Button';
+import { BrowserRouter } from 'react-router-dom';
 
+import { Button } from './components/Button';
 import { defaultTheme } from './styles/themes/default';
 import { GlobalStyle } from './styles/global';
+import { Router } from './Router';
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="danger" />
-      <Button variant="success" />
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      {/* <Button variant="primary" />
+       <Button variant="secondary" />
+       <Button variant="danger" />
+       <Button variant="success" /> */}
       <GlobalStyle />
     </ThemeProvider>
   );
