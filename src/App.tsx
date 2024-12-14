7691b8bbced43e3d9980cf0d82e62fc90c97bc40
Rafael Pelegrini <arrobarafael@hotmail.com>
2024-12-13 23:16:18 -0300

    Adição de fonts e cores dentro de tema

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ export function App() {
       <Button variant="secondary" />
       <Button variant="danger" />
       <Button variant="success" />
-      asdfasdf
+
       <GlobalStyle />
     </ThemeProvider>
   );
