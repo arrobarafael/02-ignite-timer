117503d49b9631e587fa613c2ececc7abb856aa2
Rafael Pelegrini <arrobarafael@hotmail.com>
2024-12-19 13:10:59 -0300

    Adicionado contexto global para utilizar entre rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import { ThemeProvider } from 'styled-components';
 import { BrowserRouter } from 'react-router-dom';
 
-import { Button } from './components/Button';
 import { defaultTheme } from './styles/themes/default';
 import { GlobalStyle } from './styles/global';
 import { Router } from './Router';
+import { CyclesContextProvider } from './contexts/CyclesContext';
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-        <Router />
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>
       </BrowserRouter>
 
       <GlobalStyle />
